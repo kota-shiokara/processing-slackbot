746fc4cab048b13df2ab1dd5b85c13d13be07725
kota-shiokara <kota.book.39@gmail.com>
2020-05-18 05:38:26 +0900

    first commit

---
+++ plugins/func.py
@@ -0,0 +1,10 @@
+from slackbot.bot import respond_to     # @botname: で反応するデコーダ
+from slackbot.bot import listen_to      # チャネル内発言で反応するデコーダ
+from slackbot.bot import default_reply  # 該当する応答がない場合に反応するデコーダ
+import sys
+
+@listen_to('!exit') # 使い物にならない関数
+def kill_process(message):
+    message.send('See you!')
+    print('process finished')
+    sys.exit()
