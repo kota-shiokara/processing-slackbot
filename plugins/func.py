e1c6c06acedb85e05eb7354102c65621a068886a
kota-shiokara <kota.book.39@gmail.com>
2020-05-20 01:06:23 +0900

    add func filewrite

--- plugins/func.py
+++ plugins/func.py
@@ -11,9 +11,29 @@ def kill_process(message):
 
 @listen_to('!processing (.*)')
 def processing(message, arg): # argはオプション
+    # 送信されたテキストを整形
     tmp = message.body['text']
-    pdeCode = tmp.strip("!processing " + arg + "\n")
-    print(pdeCode)
+    tmp = tmp.replace("&lt;", "<")
+    tmp = tmp.replace("&gt;", ">")
+
+    print(tmp)
+
+    pdeCode = shaping_code(tmp.strip("!processing " + arg + "\n"), arg) # pde上書き用の文字列に整形
+    #print(pdeCode)
     message.send('wait...')
-    pictFunc = "if((frameCount <= 600) && (frameCount % 15 == 0)) saveFrame(\"####.png\");\nelse exit();"
-    calcCode = pdeCode.replace("void draw(){", "void draw(){\n" + pictFunc)
+
+    # pdeに上書き
+    f = open('sketch/sketch.pde', 'w')
+    f.write(pdeCode)
+    f.close()
+
+    
+
+
+
+def shaping_code(code, option):
+    if option == '--png': # pngオプション
+        pictFunc = "  if((frameCount <= 15) && (frameCount % 15 == 0)) saveFrame(\"####.png\");\n  else exit();"
+    else: # デフォルトの動画オプション
+        pictFunc = "  if((frameCount <= 600) && (frameCount % 15 == 0)) saveFrame(\"####.png\");\n  else exit();"
+    return code.replace("void draw(){", "void draw(){\n" + pictFunc)
