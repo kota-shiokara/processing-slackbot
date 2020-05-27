903fd64d928e6fc1daacb5ff9851f9796e4465fe
kota-shiokara <kota.book.39@gmail.com>
2020-05-28 05:48:35 +0900

    fix comment and some func

--- plugins/func.py
+++ plugins/func.py
@@ -18,36 +18,39 @@ def output(message, arg): # argはオプション
     tmp = tmp.replace("&lt;", "<")
     tmp = tmp.replace("&gt;", ">")
 
-    pdeCode = shaping_code(tmp.strip("!output " + arg + "\n"), arg) # pde上書き用の文字列に整形
-    #print(pdeCode)
+    # pde上書き用の文字列に整形
+    pdeCode = shaping_code(tmp.strip("!output " + arg + "\n"), arg)
     message.send('wait...')
 
-    print(pdeCode)
     # pdeに上書き
+    print(pdeCode)
     f = open('sketch/sketch.pde', 'w')
     f.write(pdeCode)
     f.close()
 
-    cp = subprocess.run(['processing-java',  sketch_path, '--run']) # cfg.pyファイルからスケッチまでの絶対パスを入手
-    if cp.returncode != 0:
+    cp = subprocess.run(['processing-java',  sketch_path, '--run']) # processingの実行
+    if cp.returncode != 0: # processingの実行失敗時の処理
         message.send('Run is failed.')
-        sys.exit(1)
+        sys.exit(1) # なぜか機能しないsys.exit()
     
-    upload_sequence(message, arg)
+    upload_sequence(message, arg) # upload処理
+
 
 def shaping_code(code, option):
     if option == '--png': # pngオプション
         pictFunc = "  if((frameCount <= 15) && (frameCount % 15 == 0)) saveFrame(\"####.png\");\n  else if(frameCount > 15) exit();"
-    else: # デフォルトの動画オプション
+    elif option == '--gif': # gifオプション
         pictFunc = "  if((frameCount <= 300) && (frameCount % 15 == 0)) saveFrame(\"####.png\");\n  else if(frameCount > 300) exit();"
+    else : # デフォルトではpngで返す
+        pictFunc = "  if((frameCount <= 15) && (frameCount % 15 == 0)) saveFrame(\"####.png\");\n  else if(frameCount > 15) exit();"
     return code.replace("void draw(){", "void draw(){\n" + pictFunc)
 
 def upload_sequence(message, option):
-    if option == '--png':
+    if option == '--png': # pngオプション
         message.channel.upload_file(fname="sketch/0015.png", fpath="sketch/0015.png")
         if os.path.exists('sketch/0015.png'):
             os.remove('sketch/0015.png')
-    elif option == '--gif':
+    elif option == '--gif': # gifオプション
         time.sleep(6)
         file_list = sorted(glob.glob('sketch/*.png'))  
         images = list(map(lambda file : Image.open(file) , file_list))
@@ -58,5 +61,8 @@ def upload_sequence(message, option):
                 if os.path.isfile(p):
                     os.remove(p)
             os.remove('sketch/output.gif')
-
+    else : # デフォルトではpngでupload
+        message.channel.upload_file(fname="sketch/0015.png", fpath="sketch/0015.png")
+        if os.path.exists('sketch/0015.png'):
+            os.remove('sketch/0015.png')
 
